b9f22388c89885d6ce421b227c938da61c9578bf
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-12-31 01:22:59 -0300

    fix: 🐛 image margin at list

--- src/components/shared/PhotoPlaces.tsx
+++ src/components/shared/PhotoPlaces.tsx
@@ -1,6 +1,11 @@
 
+import Image from 'next/image'
 const ImagePlaces= ({src, alt}) => {
+
+
+
     return <img src={src} alt={alt} loading='lazy'  width="100%" height="200px" style={{marginTop: '1rem'}} />;
+    
   }
 
 export default ImagePlaces;
